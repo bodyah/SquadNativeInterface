0c59609b071dd467be65cbec5ae0661ed278621a
__asm <94188154+bodyah@users.noreply.github.com>
2022-11-06 10:56:20 +0200

    Add files via upload

---
+++ NativeBootstrapper/NativeBootstrapper/NativeBootstrapper.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+typedef BOOL(__stdcall* ExceptionHandlerProtection)(); //or SquadNativeInterface::ExceptionHandlerProtection()
+
+int main()
+{
+    HINSTANCE hNative = LoadLibraryA("native.dll");
+    if (!hNative) {
+        MessageBoxA(NULL, "Error", "NativeBoot", MB_ICONERROR);
+    }
+    else {
+        MessageBoxA(NULL, "Success", "NativeBoot", MB_ICONINFORMATION);
+        ExceptionHandlerProtection nativeMethod = (ExceptionHandlerProtection)GetProcAddress(hNative, "ExceptionHandlerProtection"); //or SquadNativeInterface::ExceptionHandlerProtection()
+        nativeMethod();
+    }
+}
+
